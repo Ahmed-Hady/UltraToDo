0df62fe86ffcae4d2507735302bc9ee2124e644e
Ahmed Hady <ahmedhady6@gmail.com>
2018-09-29 17:45:02 +0200

    Implement Login fragment

--- app/src/main/java/org/ultradevs/todolist/framgments/LoginFragment.java
+++ app/src/main/java/org/ultradevs/todolist/framgments/LoginFragment.java
@@ -1,19 +1,37 @@
 package org.ultradevs.todolist.framgments;
 
 
+import android.content.ContentValues;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.ultradevs.todolist.R;
+import org.ultradevs.todolist.activities.MainActivity;
+import org.ultradevs.todolist.helpers.db_helper;
+import org.ultradevs.todolist.utils.UserContract;
+
+import static org.ultradevs.todolist.activities.MainActivity.LOG_TAG;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment
+        implements View.OnClickListener {
 
+    private EditText mUserName;
+    private EditText mUserPass;
+    private Button mLoginBtn;
+
+    private String name;
+    private String pass;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -25,7 +43,36 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        mUserName = (EditText) view.findViewById(R.id.log_u_name);
+        mUserPass = (EditText) view.findViewById(R.id.log_u_pass);
+        mLoginBtn = (Button) view.findViewById(R.id.LogBtn);
+
+        mLoginBtn.setOnClickListener(this);
+
         return  view;
     }
 
+    private db_helper mUsersDbHelper;
+
+    @Override
+    public void onClick(View v) {
+
+        name = mUserName.getText().toString();
+        pass = mUserPass.getText().toString();
+
+        mUsersDbHelper = new db_helper(getContext());
+
+        String sql = String.format("UPDATE " + UserContract.USERS_ENTERY.TABLE_NAME + " SET " +
+                UserContract.USERS_ENTERY.COLUMN_STATUS + "=1 WHERE " + UserContract.USERS_ENTERY.COLUMN_NAME + "='" + name +
+                "' AND " + UserContract.USERS_ENTERY.COLUMN_PASSWORD + "='" + pass + "'");
+
+        SQLiteDatabase sqlDB = mUsersDbHelper.getWritableDatabase();
+
+        try {
+            sqlDB.execSQL(sql);
+        }catch (SQLException e){
+            Log.d(LOG_TAG, e.getMessage());
+        }
+    }
 }
